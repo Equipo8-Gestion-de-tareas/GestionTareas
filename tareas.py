20d0cdb283fcd7163d78b729ddf2bc857b2b1638
b-28282 <benjamin.saez@sansano.usm.cl>
2024-09-03 00:14:41 -0400

    encriptado incompleto
    encripta las tareas guardadas, pero aun no utiliza la contraseña

--- tareas.py
+++ tareas.py
@@ -1,7 +1,10 @@
 import json
+from cryptography.fernet import Fernet
 
 tareas = {}
 
+#cambiar por funcion!
+f = Fernet("o4pXx1VoKQGRM-7MQGLfpOYuN-G2RHfal4qHrUPpM74=")
 
 def agregar_tarea(titulo: str, descripcion: str, fecha_vencimiento: str, etiqueta: str):
     tarea = {}
@@ -24,8 +27,11 @@ def get_tarea_id_by(criterio: str, valor: str):
     return None
 
 def guardar_tareas():
-    with open("tareas.json", "w") as file:
-        json.dump(tareas, file)
+    json_tareas = json.dumps(tareas)
+    encryp = f.encrypt(bytes(json_tareas, encoding="utf-8"))
+    with open("./db/tareas.enc", "w") as file:
+        file.write(encryp.decode("utf-8"))
+        
 
 def _int_keys(d):
     try:
@@ -36,5 +42,8 @@ def _int_keys(d):
 
 def cargar_tareas():
     global tareas
-    with open("tareas.json", "r") as file:
-        tareas = json.load(file, object_hook=_int_keys)
+    with open("./db/tareas.enc", "r") as file:
+        encryp = file.read()
+    encryp = bytes(encryp, encoding="utf-8")
+    dec = f.decrypt(encryp).decode("utf-8")
+    tareas = json.loads(dec, object_hook=_int_keys)
