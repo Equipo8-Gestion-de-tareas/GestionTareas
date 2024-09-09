ba561e5dd504951b6ffe9e0d0d76949be7793851
b-28282 <benjamin.saez@sansano.usm.cl>
2024-09-09 01:42:41 -0300

    Funcionalidad completa
    Funcionalidad terminada de CRUD de tareas

--- tareas.py
+++ tareas.py
@@ -40,6 +40,49 @@ def input_fecha_ui(mensaje: str, permitir_vacio: bool = False) -> datetime:
             print(Fore.RED + "Formato incorrecto, por favor reintentar." + Style.RESET_ALL)
     return fecha
 
+def get_tarea_por_id_ui():
+    incorrecto = True
+    while incorrecto:
+        try:
+            id = int(input("Ingrese ID de tarea: "))
+            tarea = tareas[id]
+            incorrecto = False
+        except ValueError:
+            logging.warning("Input de ID incorrecto del usuario")
+            print(Fore.RED + "ID incorrecto, por favor reintentar." + Style.RESET_ALL)
+        except KeyError:
+            logging.warning("Usuario intenta acceder tarea inexistente")
+            print(Fore.RED + "ID no existe, por favor reintentar." + Style.RESET_ALL)
+    return tarea
+
+def editar_campo_ui(campo: str, tarea):
+    match campo:
+        case "titulo":
+            nuevo = input("Ingrese nuevo título: ")
+        case "descripcion":
+            nuevo = input("Ingrese nueva descripción")
+        case "fecha_vencimiento":
+            nuevo = input_fecha_ui("Ingrese nueva fecha de vencimiento: ")
+        case "etiqueta":
+            nuevo = input("Ingrese nueva etiqueta")
+        case "estado":
+            nuevo = input("Enter para avanzar estado de progreso.")
+    editar_campo(tarea, campo, nuevo)
+    menu_principal()
+
+def editar_tarea_ui():
+    tarea = get_tarea_por_id_ui()
+    menu_edit = Menu(f"Elegir campo a editar en tarea {tarea['id']}")
+    menu_edit.add_options([
+    ("Título", lambda: editar_campo_ui("titulo", tarea)),
+    ("Descripción", lambda: editar_campo_ui("descripcion", tarea)),
+    ("Fecha de vencimiento", lambda: editar_campo_ui("fecha_vencimiento", tarea)),
+    ("Etiqueta", lambda: editar_campo_ui("etiqueta", tarea)),
+    ("Estado de progreso", lambda: editar_campo_ui("estado", tarea)),
+    ("Volver", menu_principal)
+    ])
+    menu_edit.show()
+
 def agregar_tarea_ui():
     titulo = input("Ingrese título: ")
     desc = input("Ingrese descripción: ")
@@ -51,9 +94,8 @@ def agregar_tarea_ui():
 def mostrar_tareas_ui():
     if len(tareas) == 0:
         print("No hay tareas.")
-    for key, value in tareas.items():
-        print(f"\n[ID {key}]")
-        print_tarea(value)
+    for tarea in tareas.values():
+        print_tarea(tarea)
     input("Presiona enter para continuar.")
     menu_principal()
 
@@ -84,7 +126,9 @@ def buscar_tarea_ui():
     ("Título", lambda: resultado_busqueda_ui("titulo")),
     ("Descripción", lambda: resultado_busqueda_ui("descripcion")),
     ("Rango de fechas", lambda: resultado_rango_fechas_ui()),
-    ("Etiqueta", lambda: resultado_busqueda_ui("etiqueta"))
+    ("Etiqueta", lambda: resultado_busqueda_ui("etiqueta")),
+    ("Estado de progreso", lambda: resultado_busqueda_ui("estado")),
+    ("Volver", menu_principal)
     ])
     menu_busqueda.show()
 
@@ -105,6 +149,7 @@ def parse_fecha(fecha: str) -> datetime:
     return datetime.strptime(fecha, "%Y-%m-%d")
 
 def print_tarea(tarea):
+    print(f'\n[ID {tarea["id"]}]')
     print(f'  Título: {tarea["titulo"]}')
     print(f'  Descripción: {tarea["descripcion"]}')
     print(f'  Fecha de vencimiento: {tarea["fecha_vencimiento"]}')
@@ -113,6 +158,14 @@ def print_tarea(tarea):
         print(Fore.RED + "ATRASADA" + Style.RESET_ALL)
     else:
         print(Fore.GREEN + "A tiempo" + Style.RESET_ALL)
+    match tarea["estado"]:
+        case "pendiente":
+            print( "Estado: " + Fore.YELLOW + "Pendiente" + Style.RESET_ALL)
+        case "progreso":
+            print( "Estado: " + Fore.LIGHTBLUE_EX + "En progreso" + Style.RESET_ALL)
+        case "completada":
+            print( "Estado: " + Fore.LIGHTGREEN_EX + "Completada" + Style.RESET_ALL)
+    print("")
 
 
 def agregar_tarea(titulo: str, descripcion: str, fecha_vencimiento: datetime, etiqueta: str):
@@ -122,14 +175,32 @@ def agregar_tarea(titulo: str, descripcion: str, fecha_vencimiento: datetime, et
     tarea["descripcion"] = descripcion
     tarea["fecha_vencimiento"] = fecha_vencimiento.strftime("%Y-%m-%d")
     tarea["etiqueta"] = etiqueta
+    tarea["estado"] = "pendiente"
     if not tareas:
+        tarea["id"] = 0
         tareas[0] = tarea
         return
     ultimo_id = list(tareas.keys())[-1]
-    tareas[ultimo_id + 1] = tarea
+    id = ultimo_id + 1
+    tareas[id] = tarea
+    tarea["id"] = id
     logging.info("Tarea nueva agregada")
     guardar_tareas()
 
+def editar_campo(tarea, campo: str, valor):
+    if campo == "estado":
+        if tarea[campo] == "pendiente":
+            tarea[campo] = "progreso"
+        elif tarea[campo] == "progreso":
+            tarea[campo] = "completada"
+        else:
+            log.warning("Usuario intenta avanzar tarea completada")
+    elif isinstance(valor, datetime):
+        tarea[campo] = valor.strftime("%Y-%m-%d")
+    else:
+        tarea[campo] = valor
+    logging.info(f"Tarea {tarea['id']} editada")
+    guardar_tareas()
 
 def get_tareas_by(criterio: str, valor: str):
     global tareas
@@ -176,19 +247,22 @@ def salir():
     log.info("Aplicacion cerrada")
     quit()
 
-set_user_data("woww", "woww")
-
 menu = Menu("Gestión de tareas")
 menu.add_options([
-    ("Agregar tareas", agregar_tarea_ui),
+    ("Agregar tarea", agregar_tarea_ui),
     ("Mostrar todas las tareas", mostrar_tareas_ui),
     ("Buscar tareas", buscar_tarea_ui),
+    ("Editar tarea por ID", editar_tarea_ui),
     ("Salir", salir)
 ])
 
 def menu_principal():
     menu.show()
 
-cargar_tareas()
-guardar_tareas()
-menu_principal()
+def init(name: str, passw: str):
+    set_user_data(name, passw)
+    cargar_tareas()
+    guardar_tareas()
+    menu_principal()
+
+init("woww", "woww")
